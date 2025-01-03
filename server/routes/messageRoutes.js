b9deacb74327e38c145580dda1b12a05391945be
MrslvSol <sikler722@gmail.com>
2025-01-03 09:30:22 +0300

    Delete server directory

--- server/routes/messageRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getMessages, sendMessage } = require('../controllers/messageController');
-const router = express.Router();
-
-router.get('/', getMessages);
-router.post('/', sendMessage);
-
-module.exports = router;
